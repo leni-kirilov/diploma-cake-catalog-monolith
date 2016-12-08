d38f862e258400539ff25b32f597ce22b1cb4dcf
Leni Kirilov <Leni.Kirilov@gmail.com>
2016-12-08 22:07:21 +0200

    matching any cake-catalog-monolith war file for liquibase execution

--- webapp-runner.sh
+++ webapp-runner.sh
@@ -2,10 +2,10 @@ echo ''
 echo 'Executing DB migrations'
 
 java -jar ./server/liquibase-core-3.5.3.jar \
---driver=org.postgresql.Driver \
---classpath=./build/libs/*.war \
---changeLogFile=changelog.xml \
---url="$(echo $JDBC_DATABASE_URL)&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" migrate;
+    --driver=org.postgresql.Driver \
+    --classpath=$(ls build/libs/cake-catalog-monolith*.war) \
+    --changeLogFile=changelog.xml \
+    --url="$(echo $JDBC_DATABASE_URL)&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" migrate;
 
 echo ''
 echo 'Running app...'
