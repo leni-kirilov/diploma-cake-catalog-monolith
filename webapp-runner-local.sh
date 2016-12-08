cf065d83bbb23143b8e0722c7984a4d7085164b4
Leni Kirilov <Leni.Kirilov@gmail.com>
2016-12-08 22:20:30 +0200

    fixy fix 2

--- webapp-runner-local.sh
+++ webapp-runner-local.sh
@@ -5,9 +5,10 @@ gradle stage
 echo ''
 echo 'Executing DB migrations'
 
-java -jar ./server/liquibase-core-3.5.3.jar \
+cd build/libs ;
+java -jar ../../server/liquibase-core-3.5.3.jar \
      --driver=com.mysql.jdbc.Driver \
-     --classpath=$(ls ./build/libs/cake-catalog-monolith*.war) \
+     --classpath=$(ls *.war) \
      --changeLogFile=changelog.xml \
      --url="jdbc:mysql://localhost:3306/cake_catalog_monolith" \
      --username=root \
@@ -16,4 +17,7 @@ java -jar ./server/liquibase-core-3.5.3.jar \
 
 echo ''
 echo 'Running app...'
-cd build/libs ; java $JAVA_OPTS -Dgrails.env=dev -jar ../../server/webapp-runner.jar --port 8000 *.war
+
+java $JAVA_OPTS -Dgrails.env=dev -jar ../../server/webapp-runner.jar --port 8000 *.war
+
+cd ../..
