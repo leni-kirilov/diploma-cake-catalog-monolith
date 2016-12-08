e1801073366c615b9da3acbf751a689b666eb221
Leni Kirilov <Leni.Kirilov@gmail.com>
2016-12-08 22:01:14 +0200

    fix .war localtion in liquibase script

--- webapp-runner.sh
+++ webapp-runner.sh
@@ -3,7 +3,7 @@ echo 'Executing DB migrations'
 
 java -jar ./server/liquibase-core-3.5.3.jar \
 --driver=org.postgresql.Driver \
---classpath=build/libs/cake-catalog-monolith-0.1.war \
+--classpath=./build/libs/*.war \
 --changeLogFile=changelog.xml \
 --url="$(echo $JDBC_DATABASE_URL)&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" migrate;
 
