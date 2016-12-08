7f23ab2e9ac92131d73130528da9916c567b5b80
Leni Kirilov <Leni.Kirilov@gmail.com>
2016-12-08 20:15:32 +0200

    Checkout procfile java_opts

--- Procfile
+++ Procfile
@@ -1,1 +1,12 @@
-web: cd build/libs ; java $JAVA_OPTS -Dgrails.env=prod -jar ../../server/webapp-runner.jar --port $PORT *.war
+web:
+
+echo $JAVA_OPTS
+java -jar ./target/tomcat.8000/work/Tomcat/localhost/_/WEB-INF/lib/liquibase-core-3.5.3.jar \
+     --driver=org.postgresql.Driver \
+     --classpath=build/libs/cake-catalog-monolith-0.1.war \
+     --changeLogFile=changelog.xml \
+     --url="jdbc:postgresql://ec2-54-75-230-140.eu-west-1.compute.amazonaws.com:5432/d3bifn5ooqrvhe?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" \
+     --username=kgmevmhljblkht \
+     --password= \
+     migrate
+cd build/libs ;
--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,13 +1,14 @@
 environments {
-    production {
-        dataSource {
-            dbCreate = "update"
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQL94Dialect
-            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
-        }
+  production {
+    dataSource {
+      dbCreate = "update"
+      driverClassName = "org.postgresql.Driver"
+      dialect = org.hibernate.dialect.PostgreSQL94Dialect
+      uri = new URI(System.env.DATABASE_URL ?: "postgres://test:test@localhost/test")
+//      url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+//      url = "jdbc:postgresql://ec2-54-75-230-140.eu-west-1.compute.amazonaws.com:5432/d3bifn5ooqrvhe?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+      username = uri.userInfo.split(":")[0]
+      password = uri.userInfo.split(":")[1]
     }
+  }
 }
