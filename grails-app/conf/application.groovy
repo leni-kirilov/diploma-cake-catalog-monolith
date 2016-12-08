0be6d0f3c996a2abb6659578333939e4b952b79e
Leni Kirilov <Leni.Kirilov@gmail.com>
2016-12-08 23:30:50 +0200

    fix production jdbc urls

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,14 +1,12 @@
 environments {
   production {
     dataSource {
-      dbCreate = "update"
       driverClassName = "org.postgresql.Driver"
       dialect = org.hibernate.dialect.PostgreSQL94Dialect
-      uri = new URI(System.env.DATABASE_URL ?: "postgres://test:test@localhost/test")
-//      url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-//      url = "jdbc:postgresql://ec2-54-75-230-140.eu-west-1.compute.amazonaws.com:5432/d3bifn5ooqrvhe?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
-      username = uri.userInfo.split(":")[0]
-      password = uri.userInfo.split(":")[1]
+//      uri = new URI("postgres://kgmevmhljblkht:LEUEvRKfyeiNuOmUZDPshGGAqs@ec2-54-75-230-140.eu-we?
+      url = 'jdbc:postgresql://ec2-54-75-230-140.eu-west-1.compute.amazonaws.com:5432/d3bifn5ooqrvhe?user=kgmevmhljblkht&password=LEUEvRKfyeiNuOmUZDPshGGAqs&sslmode=require'
+//      username = uri.userInfo.split(":")[0]
+//      password = uri.userInfo.split(":")[1]
     }
   }
 }
--- webapp-runner-local.sh
+++ webapp-runner-local.sh
@@ -18,6 +18,6 @@ java -jar ../../server/liquibase-core-3.5.3.jar \
 echo ''
 echo 'Running app...'
 
-java $JAVA_OPTS -Dgrails.env=dev -jar ../../server/webapp-runner.jar --port 8000 *.war
+java $JAVA_OPTS -Dgrails.env=development -jar ../../server/webapp-runner.jar --port 8000 *.war
 
 cd ../..
