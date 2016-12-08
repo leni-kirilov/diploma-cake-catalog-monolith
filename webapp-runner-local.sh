6e45f3ad2ca5e7fbf33f8098ebe19c113a7328a4
Leni Kirilov <Leni.Kirilov@gmail.com>
2016-12-08 21:57:08 +0200

    added .sh file for the execution local and on Production
    liquibase jar inlined in code

---
+++ webapp-runner-local.sh
@@ -0,0 +1,19 @@
+echo ''
+echo 'Building war...'
+gradle stage
+
+echo ''
+echo 'Executing DB migrations'
+
+java -jar ./server/liquibase-core-3.5.3.jar \
+     --driver=com.mysql.jdbc.Driver \
+     --classpath=build/libs/cake-catalog-monolith-0.1.war \
+     --changeLogFile=changelog.xml \
+     --url="jdbc:mysql://localhost:3306/cake_catalog_monolith" \
+     --username=root \
+     --password= \
+     migrate
+
+echo ''
+echo 'Running app...'
+cd build/libs ; java $JAVA_OPTS -Dgrails.env=dev -jar ../../server/webapp-runner.jar --port 8000 *.war
---
+++ webapp-runner.sh
@@ -0,0 +1,14 @@
+echo ''
+echo 'Executing DB migrations'
+
+java -jar ./server/liquibase-core-3.5.3.jar \
+--driver=org.postgresql.Driver \
+--classpath=build/libs/cake-catalog-monolith-0.1.war \
+--changeLogFile=changelog.xml \
+--url="$(echo $JDBC_DATABASE_URL)&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" migrate;
+
+echo ''
+echo 'Running app...'
+
+cd build/libs ;
+java $JAVA_OPTS -Dgrails.env=prod -jar ../../server/webapp-runner.jar --port $PORT *.war
