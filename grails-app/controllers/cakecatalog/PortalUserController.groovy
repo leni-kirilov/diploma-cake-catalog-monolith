fc9fe65dd00ec2adf52a7200f4f45e117afdc63b
Leni Kirilov <Leni.Kirilov@gmail.com>
2017-01-01 16:02:02 +0200

    Improved Logging
    - auth client now propery created with sourceId = MONOLITH
    - a few more checks

--- grails-app/controllers/cakecatalog/PortalUserController.groovy
+++ grails-app/controllers/cakecatalog/PortalUserController.groovy
@@ -2,19 +2,25 @@ package cakecatalog
 
 import com.cakecatalog.srv.auth.client.AuthClient
 import com.cakecatalog.srv.auth.client.model.PortalUserResponse
-import grails.transaction.Transactional
 import grails.validation.ValidationErrors
 
 import static org.springframework.http.HttpStatus.CREATED
 import static org.springframework.http.HttpStatus.NOT_FOUND
 import static org.springframework.http.HttpStatus.OK
 
-@Transactional(readOnly = true)
 class PortalUserController {
 
   AuthClient authClient
 
   def show(PortalUser portalUser) {
+    if (session['loggedUser']?.id != portalUser?.id) {
+      flash.message = "Cannot view this page as you don't own it."
+      redirect(
+        action: 'index',
+        controller: 'cake',
+      )
+      return
+    }
     respond portalUser
   }
 
@@ -22,9 +28,9 @@ class PortalUserController {
     respond new PortalUser(params)
   }
 
-  @Transactional
   def save(PortalUser portalUser) {
     if (portalUser == null) {
+      log.info "No user $portalUser found"
       notFound()
       return
     }
@@ -43,6 +49,7 @@ class PortalUserController {
       }
 
     } catch (Exception e) {
+      log.info e
       respond new ValidationErrors(createdPortalUser), view: 'create'
       return
     }
@@ -52,7 +59,7 @@ class PortalUserController {
     if (session['loggedUser']?.id == portalUser?.id) {
       respond portalUser
     } else {
-      flash.message = "Cannot view this page as you don't own it"
+      flash.message = "Cannot view this page as you don't own it. Your logged user${session['loggedUser']} vs required: ${portalUser?.id}"
       redirect(
         action: 'index',
         controller: 'cake',
@@ -60,7 +67,6 @@ class PortalUserController {
     }
   }
 
-  @Transactional
   def update(PortalUser portalUser) {
     if (portalUser == null) {
       notFound()
